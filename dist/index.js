8e8d23c8e072b5380f684788fa84d36a0bcd4dd8
Sébastien ROUL <sebastien.roul@icowsoft.com>
2025-06-04 17:50:27 +0200

    [.GITIGNORE] First version

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+export default function buildCheckPlugin() {
+    const buildCheck = process.env.BUILD_CHECK || new Date().toISOString();
+    return {
+        name: 'vite-plugin-build-check',
+        transformIndexHtml(html) {
+            const injectScript = `
+        <script>
+          const BUILD_CHECK = "${buildCheck}";
+          fetch('/check.json', { cache: 'no-store' })
+            .then(res => res.json())
+            .then(data => {
+              if (data.check && data.check !== BUILD_CHECK) {
+                location.reload(true);
+              }
+            })
+            .catch(console.error);
+        </script>
+      `;
+            return html.replace('</head>', `${injectScript}</head>`);
+        },
+        generateBundle() {
+            this.emitFile({
+                type: 'asset',
+                fileName: 'check.json',
+                source: JSON.stringify({ check: buildCheck })
+            });
+        }
+    };
+}
