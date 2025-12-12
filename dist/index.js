77af3da4860787c9fe6bb2c5bb516128fcfa3095
Sébastien ROUL <sebastien.roul@icowsoft.com>
2025-12-12 15:23:19 +0100

    [DIST] fix

--- dist/index.js
+++ dist/index.js
@@ -7,48 +7,49 @@ export default function buildCheckPlugin(options = {}) {
         transformIndexHtml(html) {
             const injectScript = `
         <script>
-          const BUILD_CHECK = "${buildCheck}";
-          fetch('./check.json', { cache: 'no-store' })
-            .then(res => res.json())
-            .then(data => {
-              if (data.check && data.check === BUILD_CHECK) {
-                return
-              }
-              // Event de retour pour reload
-              window.addEventListener('message', (event) => {
-                if (event.origin && (event.data.name !== 'PwaReloadToSkeletor' || event.data.trigger !== 'reload' || event.data.contextPath !== '${options.contextPath}')) {
-                  return
-                }
-                location.reload(true);
-              })
-              // Récup du SW to unregister
-              navigator.serviceWorker.getRegistrations().then(
-                (registrations) => {
-                  const ws = registrations.map(r => {
-                    if (!r.scope.includes('${options.contextPath}/')) {
-                      return null
-                    }
-                    // Sending msg popur display wait box
-                    // window.parent.postMessage({ name: 'PwaReloadToSkeletor', trigger: 'failCheck', contextPath: '${options.contextPath}'})
-                    return r.update()
-                  })
-                  // 4. Écouter le changement de contrôleur
-                  navigator.serviceWorker.addEventListener('controllerchange', () => {
-                  console.log('Reloading')
-                    window.location.reload(); // Recharge la page quand le nouveau SW est actif
-                  });
-                  return Promise.all(ws.filter(el=>el!=null))
-                },
-                (err) => {
-                  throw err
-                }
-              )
-            .then ((resp) => {
-              console.log('Reloading done')
-            }, (err) => {
-              console.error('Reloading error', error)
-            })
-          .catch(console.error);
+         const BUILD_CHECK = "${buildCheck}";
+fetch('./check.json', { cache: 'no-store' })
+  .then(res => res.json())
+  .then(data => {
+    if (data.check && data.check === BUILD_CHECK) {
+      return
+    }
+    // Event de retour pour reload
+    window.addEventListener('message', (event) => {
+      if (event.origin && (event.data.name !== 'PwaReloadToSkeletor' || event.data.trigger !== 'reload' || event.data.contextPath !== '${options.contextPath}')) {
+        return
+      }
+      location.reload(true);
+    })
+    // Récup du SW to unregister
+    navigator.serviceWorker.getRegistrations().then(
+      (registrations) => {
+        const ws = registrations.map(r => {
+          if (!r.scope.includes('${options.contextPath}/')) {
+            return null
+          }
+          // Sending msg popur display wait box
+          // window.parent.postMessage({ name: 'PwaReloadToSkeletor', trigger: 'failCheck', contextPath: '${options.contextPath}'})
+          return r.update()
+        })
+        // 4. Écouter le changement de contrôleur
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+          console.log('Reloading')
+          window.location.reload(); // Recharge la page quand le nouveau SW est actif
+        });
+        return Promise.all(ws.filter(el => el != null))
+      },
+      (err) => {
+        throw err
+      }
+    )
+      .then((resp) => {
+        console.log('Reloading done')
+      }, (err) => {
+        console.error('Reloading error', error)
+      })
+  })
+  .catch(console.error);
         </script>
       `;
             return html.replace('</head>', `${injectScript}</head>`);
