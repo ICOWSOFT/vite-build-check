c3ad93998ef1098d5d1c0a029303c94e89746ad7
Sébastien ROUL <sebastien.roul@icowsoft.com>
2025-12-12 15:32:53 +0100

    [SRC] fix

--- src/tmp.js
+++ src/tmp.js
@@ -1,20 +1,4 @@
-import type { Plugin } from 'vite';
-import { writeFileSync } from 'fs';
-import { join } from 'path';
-
-interface BuildCheckPluginOptions {
-  contextPath?: string
-}
-
-export default function buildCheckPlugin(options: BuildCheckPluginOptions = {}): Plugin {
-  const buildCheck = new Date().toISOString();
-  return {
-    name: 'vite-plugin-build-check',
-
-    transformIndexHtml(html) {
-      const injectScript = `
-        <script>
-         const BUILD_CHECK = "${buildCheck}";
+const BUILD_CHECK = "${buildCheck}";
 fetch('./check.json', { cache: 'no-store' })
   .then(res => res.json())
   .then(data => {
@@ -69,14 +53,3 @@ fetch('./check.json', { cache: 'no-store' })
       })
   })
   .catch(console.error);
-        </script>
-      `;
-      return html.replace('</head>', `${injectScript}</head>`);
-    },
-    configResolved(config) {
-      // Génère dans public/
-      const publicPath = join(config.root, 'public', 'check.json');
-      writeFileSync(publicPath, JSON.stringify({ check: buildCheck }, null, 2));
-    }
-  }
-}
